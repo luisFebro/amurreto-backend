a1dbfa82f18348dced632b9936ee2913481fe788
Luis Febro <mr.febro@gmail.com>
2021-09-09 07:17:36 -0400

    LAUNCH VERSION 2.0.0 - new profit strategy

--- controllers/strategies/profit-tracker/getProfitTrackerSignal.js
+++ controllers/strategies/profit-tracker/getProfitTrackerSignal.js
@@ -1,9 +1,9 @@
 async function getProfitTrackerSignal({ profitTracker = {}, liveCandle }) {
     const {
         watching,
-        maxPerc,
         atrLimit,
         atrUpperLimit,
+        // maxPerc,
         // atrLowerLimit,
     } = profitTracker;
 
@@ -14,11 +14,10 @@ async function getProfitTrackerSignal({ profitTracker = {}, liveCandle }) {
     const currEmaTrend = liveCandle.emaTrend;
 
     const hasPassedAtrUpperLimit = currPrice >= atrUpperLimit;
-    const atrTrends = ["bullReversal", "bull"];
-    const MAX_DIFF_SPLITTER = 0.8;
-    const condAtr =
-        !hasPassedAtrUpperLimit &&
-        (maxPerc >= MAX_DIFF_SPLITTER || atrTrends.includes(currEmaTrend));
+    const atrTrends = ["uptrend"];
+    // const MAX_DIFF_SPLITTER = 0.8;
+
+    const condAtr = !hasPassedAtrUpperLimit && atrTrends.includes(currEmaTrend);
 
     const whichStrategy = condAtr ? "atr" : "tracker";
 
@@ -30,27 +29,29 @@ async function getProfitTrackerSignal({ profitTracker = {}, liveCandle }) {
 
     return {
         whichStrategy: "tracker",
-        ...getTrackerStrategy(profitTracker),
+        ...getTrackerStrategy({ ...profitTracker, currEmaTrend }),
     };
 }
 
 // PROFIT STRATEGIES
+// tracker is automatically activate in downtrend, bullReversal and bearReversal
 function getTrackerStrategy(profitTracker) {
     const {
         maxPerc,
         isProfit,
-        diffVolat,
         diffMax,
-        // netPerc,
+        netPerc,
+        currEmaTrend,
+        // diffVolat,
         // minPerc,
     } = profitTracker;
 
-    const MAX_DIFF_VOLAT_PERC = 2.5; // diff between maxPerc and minPerc from profit
+    // MAX STOP LOSS
     // FACTS
     // 2% is about -3.000 in price including 0.60% buy/sell fees.
     // 0.4 of profit is the minimum to breakeven, thus not earning or losing anything.
-
-    const maxProfitStopLoss = !isProfit && diffVolat >= MAX_DIFF_VOLAT_PERC;
+    const MAX_STOP_LOSS_PERC = 2.5;
+    const maxProfitStopLoss = !isProfit && netPerc <= MAX_STOP_LOSS_PERC;
     if (maxProfitStopLoss) {
         return {
             signal: "SELL",
@@ -58,6 +59,33 @@ function getTrackerStrategy(profitTracker) {
             transactionPerc: 100,
         };
     }
+    // END MAX STOP LOSS
+
+    // MIN AND MAX DOWNTREND PROFIT
+    const allowedTrends = ["downtrend", "bullReversal", "bearReversal"];
+    const primaryCond = isProfit && allowedTrends.includes(currEmaTrend);
+    const minDownProfitRange = maxPerc >= 1 && maxPerc < 1.5;
+    const MAX_DIFF_MINIMUM_PROFIT = 0.4;
+
+    const isMinDowntrendProfit =
+        minDownProfitRange && diffMax >= MAX_DIFF_MINIMUM_PROFIT;
+    if (isMinDowntrendProfit) {
+        return {
+            signal: "SELL",
+            strategy: "minDownTrendProfit",
+            transactionPerc: 100,
+        };
+    }
+
+    const MAX_PROFIT_DOWNTREND_PERC = 1.5;
+    if (primaryCond && netPerc >= MAX_PROFIT_DOWNTREND_PERC) {
+        return {
+            signal: "SELL",
+            strategy: "maxDowntrendProfit",
+            transactionPerc: 100,
+        };
+    }
+    // END MIN AND MAX DOWNTREND PROFIT
 
     const MAX_DIFF_START_PROFIT = 1.5;
     const MAX_DIFF_MID_PROFIT = 1;
@@ -87,6 +115,7 @@ function getTrackerStrategy(profitTracker) {
     return { signal: null };
 }
 
+// automatically activate in an uptrend
 function getAtrStrategy(profitTracker) {
     const {
         atrLowerLimit,
@@ -96,7 +125,7 @@ function getAtrStrategy(profitTracker) {
         // atrLimit,
     } = profitTracker;
     const minRangeForSellNetPerc = maxPerc >= 1.5;
-    const condMinimizeLoss = minRangeForSellNetPerc && netPerc <= 0;
+    const condMinimizeLoss = minRangeForSellNetPerc && netPerc <= -1;
 
     const atrSellCond = condMinimizeLoss || currPrice <= atrLowerLimit;
 
