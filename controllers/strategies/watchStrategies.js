1c6442c6bdd8a81a9de259dc473dbb5aeeecf525
Luis Febro <mr.febro@gmail.com>
2021-09-13 02:06:23 -0400

    LAUNCH VERSION 2.10.0 - implementation of LIMIT ORDER for low fees benefits and dynamic order type - LIMIT/MARKET - new dynamic fee switching

--- controllers/strategies/watchStrategies.js
+++ controllers/strategies/watchStrategies.js
@@ -45,7 +45,7 @@ async function watchStrategies(options = {}) {
     console.log("profitStrategy", profitStrategy);
 
     const essentialData = {
-        signal: "SELL",
+        signal: "BUY",
         strategy: "teste 40",
         transactionPerc: 100,
     };
